3822b51ad3ee568864544668f3825105f258aaf3
Antoine Coulon <antoine.coulon@epita.fr>
2020-07-22 03:07:00 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import get_user_info
 
 def main():
     while True:
-        print("MIRADOR_AUTH".center())
+        print("MIRADOR_AUTH v0.0.1")
         exec(open("./MFRC522-python/Read.py").read())
         pretty_print(get_user_info())
 
