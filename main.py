2d947a67d8b04e23f37121718c4d8d4f6f341466
Antoine Coulon <antoine.coulon@epita.fr>
2020-07-22 03:26:44 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ import user
 
 
 def main():
-    while True:
-        print("MIRADOR_AUTH v0.0.1")
+	while True:
+		print("MIRADOR_AUTH v0.0.1")
 
-        if "BOUTON PRESSE":
-            new_id = user.create()
-            print("Create new User with ID " + str(new_id)
-
-        #data = RFID.read()
-        data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        id = RFID.get_id(data)
-        user.increment_visit(id)
-        pretty_print(user.get_info(id))
+		if "BOUTON PRESSE":
+			new_id = user.create()
+			print("Create new User with ID " + str(new_id))
+		#data = RFID.read()
+		data = "3948328492384239483927584975847"
+		id = RFID.get_id(data)
+		user.increment_visit(id)
+		pretty_print(user.get_info(id))
 main()
