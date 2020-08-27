948b5bff74c875b219a1d0e0d38066c8651b7a5b
Antoine Coulon <antoine.coulon@epita.fr>
2020-08-27 18:36:34 +0200

    asd

--- src/RFID.py
+++ src/RFID.py
@@ -22,13 +22,19 @@ def read(reader):
     print("waiting for a card to be scanned...\n")
     reader = SimpleMFRC522()
 
-    with open(os.devnull, "w") as devnull:
-        old_stdout = sys.stdout
-        sys.stdout = devnull
-        try:
-            id, data = reader.read()
-        finally:
-            sys.stdout = old_stdout
+    import sys
+    import os
+
+    def foo():
+        print "abc"
+
+    old_stdout = sys.stdout
+    sys.stdout = open(os.devnull, "w")
+    try:
+        id, data = reader.read()
+    finally:
+        sys.stdout.close()
+        sys.stdout = old_stdout
 
 
     return (id, data)
