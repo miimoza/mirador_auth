a4b758de56987cf620201fc9eddf0041a66dd3a0
Antoine Coulon <antoine.coulon@epita.fr>
2020-08-31 01:27:28 +0200

    test

--- src/ranking_board.py
+++ src/ranking_board.py
@@ -1,6 +1,14 @@
 import display
+import os
+import database
 
 def dump():
     for i in range(3, 30):
         display.print_n(50, i, '|')
     display.print_n(51, 3, "------- RANKING BOARD -------")
+
+
+
+
+    for filename in os.listdir('./database/'):
+        print(filename)
