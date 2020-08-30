079ffe2255932bc455669fb402048ca5c8f9978d
Antoine Coulon <antoine.coulon@epita.fr>
2020-08-31 01:18:34 +0200

    test

--- src/ranking_board.py
+++ src/ranking_board.py
@@ -2,5 +2,5 @@ import display
 
 def dump():
     for i in range(0, 20):
-        print_n(40, i, '|')
-    print_n(50, 4, "RANKING BOARD")
+        display.print_n(40, i, '|')
+    display.print_n(50, 4, "RANKING BOARD")
