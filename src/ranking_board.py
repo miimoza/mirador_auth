55ea170ff3b60850e72d8298aefa922d09c49052
Antoine Coulon <antoine.coulon@epita.fr>
2020-08-31 01:35:18 +0200

    test

--- src/ranking_board.py
+++ src/ranking_board.py
@@ -7,9 +7,6 @@ def dump():
         display.print_n(50, i, '|')
     display.print_n(51, 3, "------- RANKING BOARD -------")
 
-
-
-
     for id in os.listdir('./database/profiles'):
         userinfo = user.get_info(id)
         print(userinfo["name"] + ":" + userinfo["nb_visit"])
