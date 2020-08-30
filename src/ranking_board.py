107e66d9f952e7a6743c5b8f762ff1c78b4dcf76
Antoine Coulon <antoine.coulon@epita.fr>
2020-08-31 01:57:46 +0200

    test

--- src/ranking_board.py
+++ src/ranking_board.py
@@ -18,8 +18,9 @@ def dump():
 
         userinfo_list.append({userinfo["name"], userinfo["grade"], userinfo["nb_visit"]})
 
-    ranking_board = sorted(userinfo_list, key=itemgetter('nb_visit'))
-    print(ranking_board)
+    print(userinfo_list)
+    #ranking_board = sorted(userinfo_list, key=itemgetter('nb_visit'))
+    ranking_board = []
 
     for userinfo in ranking_board:
         display.print_n(51, line, userinfo["name"] + ": " + userinfo["grade"] + '(' + str(userinfo["nb_visit"]) + ')')
