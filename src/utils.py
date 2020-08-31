8403af8a7033c50ea815588659939244d37b4f53
Antoine Coulon <antoine.coulon@epita.fr>
2020-08-31 02:23:35 +0200

    test

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 # UTILS
 
 def log(filename, line):
-    with open("./database/logs/" + filename, 'a') as file:
+    with open("./database/logs/" + filename, 'a+') as file:
         file.write(line)
